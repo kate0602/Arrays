e165815f53bf848e4ad7d2ce0e3f4d9f8a06e812
kate0602 <burm-ekaterina@yandex.ru>
2023-04-09 19:38:17 +0400

    original

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -8,9 +8,36 @@ void main()
 	const int n = 10;
 	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
 
-	for (int i = 0; i < n; i++)
+	//Вывод исходного кода
+	int number;
+	cout << "Количество сдвигов: "; cin >> number;
+	for (int i = 0; i < number; i++)
+	{
 
+	
+	for (int i = 0; i < n; i++)
+	{
 		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	// Сдвиг массива
+	int buffer = arr[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = arr[i + 1]; //В текущий элемент массива ложим следующий элемент
+	}
+	arr[n - 1] = buffer;
+	}
+
+	//Вывод сдвинутого кода
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 
 
 }
